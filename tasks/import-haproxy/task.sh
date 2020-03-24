d124b2e557550cfdb3f141e0b3a47886340806b4
nvpnathan <nathan79@gmail.com>
2020-03-24 15:57:52 -0600

    initial commit

---
+++ tasks/import-haproxy/task.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+export OVA_ISO_PATH='./tkg-haproxy-ova'
+
+file_path=$(find $OVA_ISO_PATH/ -name "*.ova")
+
+echo "$file_path"
+
+if echo $GOVC_INSECURE == "1"
+  then echo "Insecure vCenter Cert"
+else
+  export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
+  echo "$GOVC_CA_CERT" > "$GOVC_TLS_CA_CERTS"
+fi
+
+govc import.spec "$file_path" | python -m json.tool > tkg-haproxy-import.json
+
+cat > filters <<'EOF'
+.NetworkMapping[].Network = $network
+EOF
+
+jq \
+  --arg network "$TKG_HAPROXY_PORTGROUP" \
+  tkg-haproxy-import.json > options.json
+
+cat options.json
+
+if [ -z "$VC_VM_FOLDER" ]; then
+  govc import.ova -options=options.json "$file_path"
+else
+  if [ "$(govc folder.info "$VC_VM_FOLDER" 2>&1 | grep "$VC_VM_FOLDER" | awk '{print $2}')" != "$VC_VM_FOLDER" ]; then
+    govc folder.create "$VC_VM_FOLDER"
+  fi
+  govc import.ova -folder="$VC_VM_FOLDER" -options=options.json "$file_path"
+fi
